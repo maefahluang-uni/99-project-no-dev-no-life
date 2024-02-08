45420fc5d51bb6305ba673df33be6f010e82715d
060Sakonphat <6431503060@lamduan.mfu.ac.th>
2024-02-08 13:15:44 +0700

    add assets

---
+++ lib/background.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class BaseLayout extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return  Scaffold(
+      body:  Container(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 844,
+              width: double.infinity,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/BG.jpg"),
+                    fit: BoxFit.cover),
+              ),
+            ), 
+          ]
+        )
+      )
+    );
+  }
+}
